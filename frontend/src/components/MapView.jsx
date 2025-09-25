3eacde68b9e35bc3f5cea227d5d5d89c7aabfa7c
kishor <kishorjsk2006@gmail.com>
2025-09-25 19:10:12 +0530

    Added Markers to the Map as an Overlay

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -6,6 +6,16 @@ export default function MapView() {
   const mapRef = useRef(null);
   const [area, setArea] = useState("agucha"); // Default Agucha
 
+  // Custom Markers
+  const redMarkerIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],     // size of the icon
+    iconAnchor: [12, 41],   // point of the icon which will correspond to marker's location
+    popupAnchor: [1, -34],  // point from which the popup should open relative to the iconAnchor
+    shadowSize: [41, 41]    // size of the shadow
+  });
+
   useEffect(() => {
     const map = L.map("map").fitBounds(bounds_agucha);
     mapRef.current = map; // Store the current map instance
@@ -23,6 +33,14 @@ export default function MapView() {
         attribution: "© Esri",
       }
     );
+
+    const pit_north = L.marker([25.83844516568047, 74.7434835332167], { icon: redMarkerIcon }).bindPopup("Open Pit North"),
+          pit_south = L.marker([25.82623905266493, 74.73764704656307], { icon: redMarkerIcon }).bindPopup("Open Pit South"),
+          pit_deep  = L.marker([25.834273597713928, 74.74421309402399], { icon: redMarkerIcon }).bindPopup("Open Pit Underground Access");
+
+      const markers = L.layerGroup([pit_north, pit_south, pit_deep]);
+
+
 
     const contourLayer = L.layerGroup();
 
@@ -53,8 +71,9 @@ export default function MapView() {
       .catch((err) => console.error("Error loading GeoJSON:", err));
 
     contourLayer.addTo(map)
+    markers.addTo(map)
 
-    L.control.layers({ Map: osm, Satellite: sat }, { Contour: contourLayer }).addTo(map);
+    L.control.layers({ Map: osm, Satellite: sat }, { Contour: contourLayer, Markers: markers}).addTo(map);
 
     return () => {
       map.remove();
